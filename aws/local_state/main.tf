8661703f09377a1996386abcd6f18286f81b498a
Rakeshreddy189 <144189508+Rakeshreddy189@users.noreply.github.com>
2024-06-08 17:35:47 -0400

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -1,24 +1,21 @@
+# Configure the Azure provider
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.1.0"
 }
 
-provider "aws" {
-  region  = "us-west-2"
+provider "azurerm" {
+  features {}
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Terraform_Demo"
-  }
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
 }
 
